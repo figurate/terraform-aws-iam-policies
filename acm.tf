3f9d0d239faaab8e71ad906c51ed3ca87d2592ca
Ben Fortuna <bfortuna@odecee.com.au>
2022-02-21 14:41:06 +1100

    Added principals

--- acm.tf
+++ acm.tf
@@ -1,16 +1,20 @@
 data "aws_iam_policy_document" "acm_import_certificate" {
   statement {
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type        = "AWS"
+    }
     actions = [
       "acm:ImportCertificate",
       "acm:DescribeCertificate",
       "acm:ListTagsForCertificate",
-#      "acm:DeleteCertificate",
+      "acm:DeleteCertificate",
     ]
     resources = var.resources
-    condition {
-      test     = "StringEquals"
-      values   = [var.region]
-      variable = "aws:RequestedRegion"
-    }
+#    condition {
+#      test     = "StringEquals"
+#      values   = [var.region]
+#      variable = "aws:RequestedRegion"
+#    }
   }
 }

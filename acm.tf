6f79868b05676b2a5f8f29e2e22c9f05a30933bf
Ben Fortuna <bfortuna@odecee.com.au>
2022-02-21 14:59:30 +1100

    Added certificate sid

--- acm.tf
+++ acm.tf
@@ -1,5 +1,6 @@
 data "aws_iam_policy_document" "acm_import_certificate" {
   statement {
+    sid = "CertificateImportByUser"
     principals {
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
       type        = "AWS"

df86b0ca5b9b090c7360d7d5b02f87b54f9f00e5
Ben Fortuna <bfortuna@odecee.com.au>
2022-02-21 19:10:44 +1100

    Removed policies that fail checkov assessment

--- modules/all/vars.tf
+++ modules/all/vars.tf
@@ -1,15 +1,8 @@
 variable "policies" {
   description = "Names of predefined policies to include"
   default = [
-    "acm-certificate-import",
     "application-autoscaling-fullaccess",
-    "cloudformation-stack-management",
-    "cloudwatch-log-management",
-    "cloudwatch-log-group-management",
     "codebuild-vpc-management",
-    "dynamodb-fullaccess",
-    "ec2-subnet-management",
-    "ec2-security-group-management",
     "iam-access-key-rotation",
     "iam-assumerole-account",
     "iam-group-management",
@@ -17,15 +10,8 @@ variable "policies" {
     "iam-passrole-account",
     "iam-passrole-cloudformation",
     "iam-poweruser-boundary",
-    "iam-service-linked-role-create",
-    "kms-key-management",
-    "kms-encryption-management",
     "lambda-config-management",
-    "rds-instance-management",
     "route53-zone-readonly",
-    "ssm-parameter-management",
-    "terraform-state-management",
-    "terraform-lock-management"
   ]
 }
 
